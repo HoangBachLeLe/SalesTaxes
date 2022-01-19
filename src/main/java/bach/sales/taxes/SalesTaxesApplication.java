0cbd91eb0e9f0fca37b7f6819e14447e6e6af7f2
Hoang Bach Le Le <lele100@uni-duesseldorf.de>
2022-01-19 16:34:19 +0100

    Add Checkstyle, PMD, SpotBugs

--- src/main/java/bach/sales/taxes/SalesTaxesApplication.java
+++ src/main/java/bach/sales/taxes/SalesTaxesApplication.java
@@ -1,12 +1,13 @@
-package bach.sales_taxes;
+package bach.sales.taxes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 public class SalesTaxesApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(SalesTaxesApplication.class, args);
     }
 
--- src/test/java/bach/sales/taxes/SalesTaxesApplicationTests.java
+++ src/test/java/bach/sales/taxes/SalesTaxesApplicationTests.java
@@ -1,4 +1,4 @@
-package bach.sales_taxes;
+package bach.sales.taxes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
