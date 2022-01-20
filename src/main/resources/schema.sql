cd47c58d1edd3568983c86621a00245848ca7f15
Hoang Bach Le Le <lele100@uni-duesseldorf.de>
2022-01-20 18:52:39 +0100

    Update database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 create table goods
 (
     id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    goods_name VARCHAR(20) NOT NULL,
+    goods_name VARCHAR(50) NOT NULL,
     category   VARCHAR(20) NOT NULL,
     origin     VARCHAR(20) NOT NULL,
     price      DOUBLE      NOT NULL
